1708b3035dcdfb595813a276189e881aa0454c8c
Kamol774 <chust1222@gmail.com>
2024-10-23 16:08:54 +0900

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-console.log("executed");
+import dotenv from "dotenv"; // hardoim dotenv yuqorida(birinchida) turishi shart
+dotenv.config({
+  path: process.env.NODE_ENV === "production " ? ".env.production" : ".env",
+});
+
+import mongoose from "mongoose";
+
+mongoose // MongoDB ga mongoose orqali ulanyapmiz
+  .connect(`${process.env.MONGO_URL}` as string, {})
+  .then((data) => {
+    // connection success bo'lgandan keyin  <<<EXPRESS>>> qurilyapti va u APP.ts ichida hosil qilinyapti
+    console.log("MongoDB connection succeed");
+    const PORT = process.env.PORT ?? 3003;
+  })
+  .catch((err) => console.log("Error on connection MongoDB", err));
